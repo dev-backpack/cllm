2b4ad723428143561241d540b66a9ddbb45761f0
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-02-10 17:50:38 +0900

    Add `--version` command to CLI for checking the version (#16)
    * Add --version command to CLI for checking the version
    
    * Update version command to import from pyproject.toml file

--- cllm/main.py
+++ cllm/main.py
@@ -3,13 +3,26 @@ from .set import index as set_index
 import os
 import json
 import platform
-from langchain.chains import LLMChain
+import tomllib
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import (ChatPromptTemplate, FewShotChatMessagePromptTemplate)
 
-app = typer.Typer(help="Empower your CLI experience with a command search tool driven by LLM magic!")
+from typing import Optional
+from typing_extensions import Annotated
+
+# version of the CLI
+app = typer.Typer(help="Empower your CLI experience with a command search tool driven by LLM magic!",
+                  context_settings={"help_option_names": ["-h", "--help"]}
+                  )
 
 app.add_typer(set_index.set_app, name="set", help="Set up configurations used in cllm")
+
+def version_callback(value: bool):
+    if value:
+        with open("./pyproject.toml", "rb") as f:
+            data = tomllib.load(f)
+            typer.echo(f"cllm version: {data['tool']['poetry']['version']}")
+        raise typer.Exit()
 
 @app.command()
 def search(query : str):
@@ -65,6 +78,12 @@ def search(query : str):
     output = model.invoke(prompt)
     
     typer.echo(output.content)
+
+@app.callback()
+def main(
+        version: Optional[bool] = typer.Option(None, "--version", "-v", callback=version_callback, help="Print cllm version")
+):
+    pass
 
 if __name__ == "__main__":
     app()
