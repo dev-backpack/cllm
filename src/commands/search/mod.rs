4d36a37e9b257fb53b44b1d25c99cb355ebe60a3
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-02-28 21:41:24 +0900

    Add set key & search commands to the CLI (#27)
    * Add set key & search commands to the CLI
    - Add set key command to the CLI
    - Add search command to the CLI
    - Add clipboard copy feature to the CLI
    - Delete "Assistant: " prefix from the output of the CLI
    - Update help message of the CLI
    - Add spinner display to the CLI when searching
    
    * Update lib.rs
    
    * Update search command

--- src/commands/search/mod.rs
+++ src/commands/search/mod.rs
@@ -1,5 +1,13 @@
 use clap::Parser;
-
+use std::env;
+use spinners::{Spinner, Spinners};
+use llm_chain::{
+    chains::conversation::Chain, executor, parameters, prompt, step::Step, 
+    options::{ModelRef, OptionsBuilder},
+    prompt::{Conversation, ChatMessageCollection},
+};
+use llm_chain_openai::chatgpt::Model;
+use cli_clipboard::{ClipboardContext, ClipboardProvider};
 #[derive(Debug, Parser)]
 #[clap(
     name = "search", 
@@ -11,7 +19,78 @@ pub struct Search {
     qeury: String,    
 }
 
+pub fn few_shot_template(list: Vec<(String, String)>) -> ChatMessageCollection<String> {
+
+    let mut ret_prompt = Conversation::new();
+
+    for (user, assistant) in &list {
+        ret_prompt = ret_prompt.with_user(user.to_string()).with_assistant(assistant.to_string());
+    }
+
+    ret_prompt
+}
+
 pub async fn handle_search(search: Search) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Searching for: {}", search.qeury);
+    
+    if !env::var("OPENAI_API_KEY").is_ok() {
+        println!("Please set your OpenAI API key using the `set key` command.");
+        return Ok(());
+    }
+
+    let mut spinner = Spinner::new(Spinners::Dots9, "Searching for the command...".into());
+
+    let model = ModelRef::from_model_name(Model::Gpt35Turbo.to_string());
+
+    let mut option_builder = OptionsBuilder::new();
+    option_builder.add_option(llm_chain::options::Opt::Model(model));
+    let options = option_builder.build();
+
+    let exec = executor!(
+        chatgpt,
+        options
+    )?;
+
+    let few_shot_examples: Vec<(String, String)> = vec![
+        ("Show all pods in k8s".to_string(), "kubectl get pods".to_string()),
+        ("Find all files recursively within the current directory that contain 'a' in their filenames.".to_string(), "find . -type f  -name '*a*' -print".to_string()),
+        ("Provide the command to build and push a Docker image from the current directory.".to_string(), "docker build -t myapp:latest --path".to_string()),
+    ];
+    
+    
+    let mut conversation = Conversation::new()
+        .with_system_template(
+            "I want you to act as generating a command for request tasks on {{os_name}}. Also please don't explain the commands, just generate the command.",
+            &parameters!{"os_name" => env::consts::OS}
+        ).unwrap();
+
+    let few_shot_prompt = few_shot_template(few_shot_examples);
+
+    conversation.append(few_shot_prompt);
+
+    let conversation = conversation
+        .with_system(
+            "Only generate the command, don't explain it".to_string()
+        );
+
+    let mut chain = Chain::new_with_message_collection(
+        &conversation
+    );
+
+    let step = Step::for_prompt_template(
+        prompt!(
+            user: "task : {{query}}"
+        )
+    );
+    let parameters = parameters!().with("query", search.qeury);
+    let res = chain.send_message(step, &parameters, &exec).await?;
+    let res = res.to_immediate().await?.as_content().to_chat().to_string();
+    let res = res.split("Assistant: ").collect::<Vec<&str>>()[1].to_string().trim().to_string();
+
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    ctx.set_contents(res.clone().to_string()).unwrap();
+    
+    spinner.stop_and_persist("✔", "Finished searching for the command and copied to your clipboard :)".into());
+
+    println!("{}", res);
     Ok(())
 }
--- src/commands/set/key/mod.rs
+++ src/commands/set/key/mod.rs
@@ -1,4 +1,8 @@
 use clap::Parser;
+use dirs;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 #[derive(Debug, Parser)]
 #[clap(
@@ -12,6 +16,28 @@ pub struct Key {
 }
 
 pub async fn handle_key(key: Key) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Setting API Key: {}", key.api_key);
+    
+    let home_dir = dirs::home_dir().unwrap();
+    let save_dir = home_dir.join(".cllm");
+    let config_path = save_dir.join("credentials.json");
+
+    if !save_dir.exists() {
+        std::fs::create_dir_all(&save_dir)?;
+    }
+
+    let mut config = if config_path.exists() {
+        let config = std::fs::read_to_string(config_path.clone())?;
+        serde_json::from_str(&config)?
+    }
+    else {
+        serde_json::json!({})
+    };
+
+    config["OPEN_AI"] = key.api_key.clone().into();
+    let config = serde_json::to_string_pretty(&config)?;
+    File::create(config_path)?.write_all(config.as_bytes())?;
+    env::set_var("OPENAI_API_KEY", key.api_key);
+
+    println!("API key set successfully.");
     Ok(())
 }
