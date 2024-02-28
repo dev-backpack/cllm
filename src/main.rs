d201294b48cf1e8981615c1a33ba53191ea931ac
ykdy3951 <ykdy3951@naver.com>
2024-02-28 18:50:40 +0900

    Add set key & search commands to the CLI - Add set key command to the CLI - Add search command to the CLI - Add clipboard copy feature to the CLI - Delete "Assistant: " prefix from the output of the CLI - Update help message of the CLI - Add spinner display to the CLI when searching

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,41 @@
 pub mod commands;
 
+use std::env;
 use clap::Parser;
 use commands::{Commands, handle_command};
+use dirs;
 
 #[derive(Debug, Parser)]
 #[clap(
     version,
-    author,
+    about="Empower your CLI experience with a command search tool driven by LLM magic!\n\
+    Github: https://github.com/dev-backpack/cllm\n\
+    If you have any questions or suggestions, feel free to open an issue on the github repo."
 )]
 struct Cli {
     #[clap(subcommand)]
     pub commands: Commands,
 }
 
-
 #[tokio::main]
 async fn main() {
+
+    // Set the OPENAI_API_KEY environment variable
+    let home_dir = dirs::home_dir().unwrap();
+    let save_dir = home_dir.join(".cllm");
+    let config_path = save_dir.join("credentials.json");
+
+    if config_path.exists() {
+        let config = std::fs::read_to_string(config_path).unwrap();
+        let config: serde_json::Value = serde_json::from_str(&config).unwrap();
+
+        if config["OPEN_AI"].is_string() {
+            let api_key = config["OPEN_AI"].as_str().unwrap();
+            env::set_var("OPENAI_API_KEY", api_key);
+        }
+    }
+
+    // Parse the command line arguments
     let cli = Cli::parse();
 
     if let Err(_error) = handle_command(cli.commands).await {
