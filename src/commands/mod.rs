9a4ce485d874c8a7dfd265e4a55475f40972a5f2
Chanism <48588531+ykdy3951@users.noreply.github.com>
2024-03-06 15:55:58 +0900

    Add command to store and display previous search history in the database (#36)
    * Add SQLite, tabled, and terminal_size dependencies, and implement history command
    
    * Add query parameter to history command
    
    * Fix binding values in handle_history function
    
    * Refactor history module and fix import order
    
    * Add offset parameter to history command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,4 @@
+mod history;
 mod search;
 mod set;
 
@@ -7,11 +8,13 @@ use clap::Subcommand;
 pub enum Commands {
     Search(search::Search),
     Set(set::Set),
+    History(history::History),
 }
 
 pub async fn handle_command(command: Commands) -> Result<(), Box<dyn std::error::Error>> {
     match command {
         Commands::Search(search) => search::handle_search(search).await,
         Commands::Set(set) => set::handle_set(set).await,
+        Commands::History(history) => history::handle_history(history).await,
     }
 }
